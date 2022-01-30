39861d0e431aa4685b210d79e16fcae3273e2ba1
Akash Ravi Prame <57642848+akash711@users.noreply.github.com>
2022-01-30 16:57:58 +0100

    Update utils.py

--- dags/utils.py
+++ dags/utils.py
@@ -5,19 +5,33 @@ import requests
 import json
 import pandas as pd
 import numpy as np
-import holidays
 import pickle
 from airflow.models import Variable
+import datetime
+import os
 
-CUR_DIR = os.path.abspath(os.path.dirname(__file__))
+from airflow.hooks.postgres_hook import PostgresHook
+
+config_path = Variable.get("config_path")
 
 
-def config(filename='database.ini', section='postgresql'):
+nl_holidays = [datetime.date(2022, 1, 1),
+ datetime.date(2022, 4, 15),
+ datetime.date(2022, 4, 17),
+ datetime.date(2022, 4, 18),
+ datetime.date(2022, 4, 27),
+ datetime.date(2022, 5, 26),
+ datetime.date(2022, 6, 5),
+ datetime.date(2022, 6, 6),
+ datetime.date(2022, 12, 25),
+ datetime.date(2022, 12, 26)]
+ 
+ 
+def config(filename=f"{config_path}database.ini", section='postgresql'):
 
     """
     Parse config details from database.ini file
     """
-    filename = f"{CUR_DIR}/database.ini"
     parser = ConfigParser()
     parser.read(filename)
 
@@ -30,6 +44,7 @@ def config(filename='database.ini', section='postgresql'):
     else:
         raise Exception('Section {0} not found in the {1} file'.format(section, filename))
 
+    
     return db
 
 
@@ -41,13 +56,14 @@ def connect_to_db():
 
     db_config = config()
 
-
     try:
+        
         # Connect to an existing database
         connection = psycopg2.connect(user = db_config['user'],
                                       password = db_config['password'],
                                       host = db_config['host'],
                                       database = db_config['database'])
+
 
         # Create a cursor to perform database operations
         cursor = connection.cursor()
@@ -59,50 +75,62 @@ def connect_to_db():
         # Fetch result
         record = cursor.fetchone()
         print("You are connected to - ", record, "\n")
+        
+        return cursor, connection
+    
     
     except (Exception, Error) as error:
         print("Error while connecting to PostgreSQL", error)
 
-    return cursor, connection
+    
     
         
 def create_db():
 
-    cursor, connection = connect_to_db()
-
-
-    sql_query = """
+    try:
         
-        CREATE TABLE IF NOT EXISTS Weather
-        (
-            id SERIAL PRIMARY KEY NOT NULL,
-            DateTime TIMESTAMPTZ,
-            WindSpeed INTEGER,
-            Temperature INTEGER,
-            Precipitation BOOLEAN
-        );
-        """
-
-    cursor.execute(sql_query)
-    connection.commit()
-    print('Database: Weather created!')
-    sql_query = """
+        cursor, connection = connect_to_db()
         
-        CREATE TABLE IF NOT EXISTS Traffic
-        (
-            id SERIAL PRIMARY KEY NOT NULL,
-            date_id BIGINT UNIQUE REFERENCES public.weather,
-            Intensity INTEGER
-        );
-        """
+    
+        sql_query = """
+            CREATE TABLE IF NOT EXISTS Weather
+            (
+                id SERIAL PRIMARY KEY NOT NULL,
+                DateTime TIMESTAMPTZ,
+                WindSpeed INTEGER,
+                Temperature INTEGER,
+                Precipitation BOOLEAN
+            );
+            """
+        
+        
+        
+        
+        cursor.execute(sql_query)
+        connection.commit()
+        
+        print('Database: Weather created!')
+        sql_query = """
+            
+            CREATE TABLE IF NOT EXISTS Traffic
+            (
+                id SERIAL PRIMARY KEY NOT NULL,
+                date_id BIGINT UNIQUE REFERENCES public.weather,
+                Intensity INTEGER
+            );
+            """
+        
+        cursor.execute(sql_query)
+        connection.commit()
+
+        print('Database: Traffic created!')
     
 
-    cursor.execute(sql_query)
-    connection.commit()
+        cursor.close()
+        connection.close()
 
-    print('Database: Traffic created!')
-    cursor.close()
-    connection.close()
+    except:
+        raise Exception('Could not connect to DB')
 
 
 def convert_to_celc(fahrenheit):
@@ -115,14 +143,26 @@ def extract_weather_data(location_url = 'http://dataservice.accuweather.com/loca
     api_key = 'nPQT2nMFQG3ghVYazkU5jQ0P58DsVPNn'
     
 
-    response = requests.get(location_url, params={'apikey':api_key, 'q': query, 'details': True})
+    response = requests.get(location_url, params={'apikey':api_key, 'q': query, 'details': False})
 
     
     loc_codes = json.loads(response.content)
-    for i in loc_codes:
-        if i['EnglishName'] == query:
-            location_key = i['Key']
-        break
+    
+    try:
+        if type(loc_codes) == list:   
+            for i in loc_codes:
+                if i['EnglishName'] == query:
+                    location_key = i['Key']
+                break
+        elif type(loc_codes) == dict:
+            if loc_codes['EnglishName']:
+                location_key = loc_codes['Key']
+        else:
+            raise Exception('Invalid Search Query: {}'.format(query))
+    
+    except KeyError:
+        raise Exception('Problem with API. Try again later!')
+
 
     forecast_api = f'http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/{location_key}'
     response = requests.get(forecast_api, params={'apikey':api_key, "details": True})
@@ -173,13 +213,16 @@ def insert_into_weather_db():
 
 def predict_traffic():
 
+    
     weather_data = extract_weather_data()
 
     #Extract from DB
 
-    nl_holidays = holidays.CountryHoliday('NL')
+    try:
+        model = pickle.load(open(f"{config_path}RF_trafficmodel.p", "rb"))
 
-    model = pickle.load(open( f"{CUR_DIR}/RF_trafficmodel.p", "rb" ))
+    except:
+        raise Exception('Cannot read pickle file!')
 
     df = pd.DataFrame(columns = ['Month', 'Day', 'Hour', 'DayofWeek', 'isHoliday', 'Wind Speed', 'Temperature', 'Precipitation'])
 
@@ -225,7 +268,3 @@ def insert_into_traffic_db():
     connection.close()
 
 
-
-create_db()
-insert_into_weather_db()
-insert_into_traffic_db()
